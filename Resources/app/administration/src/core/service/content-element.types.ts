9501ef5b8b86f0dc17655de435b5a6179a400df6
goxs <lyricsong47@gmail.com>
2026-09-07 15:14:05 +0800

    feat(content-system): sync latest rendering model

--- Resources/app/administration/src/core/service/content-element.types.ts
+++ Resources/app/administration/src/core/service/content-element.types.ts
@@ -1,5 +1,6 @@
 /**
  * @private
+ * @ct-package discovery
  */
 export interface ContentElementNode {
     id: string;
@@ -10,4 +11,5 @@ export interface ContentElementNode {
     slots?: Record<string, ContentElementNode[]>;
     providesContext?: unknown;
     acceptsContext?: unknown;
+    attributedSpecifications?: Record<string, string>;
 }
